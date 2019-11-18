f568bdd5c21cd79fc91aef73923d91f8d2408682
Matthew Starke <matthew.j.starke@gmail.com>
2019-11-18 17:32:36 -0600

    tweak hodograph domain

--- archived_rap_plotter.py
+++ archived_rap_plotter.py
@@ -285,7 +285,7 @@ def plot_skewt(snd: Sounding, save_to: Optional[str] = None, p_top: int = 100):
     # Hodograph
 
     # Set up axis for hodograph.
-    h = Hodograph(ax_hodo)
+    h = Hodograph(ax_hodo, component_range=100)
     h.add_grid(20)
 
     # Plot each segment individually for control over color, reversed so that the full hodograph is plotted first,
@@ -297,6 +297,9 @@ def plot_skewt(snd: Sounding, save_to: Optional[str] = None, p_top: int = 100):
 
     ax_hodo.set_xticks([])
     ax_hodo.set_yticks([])
+    ax_hodo.set_xlim(-60, 100)
+    ax_hodo.set_ylim(-60, 100)
+    ax_hodo.set_xlabel("20kt increments")
     for a in range(20, 81, 20):
         plt.text(-a * 0.71, -a * 0.71, a, ha="center", va="center")
 
